56ddad8720db3c4391971b9c15042f7d0a3f1010
Michael <3533648+hertg@users.noreply.github.com>
2022-02-20 18:06:07 +0100

    add umlauts

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ KEYMAP = $(word 2, $(ARG))
 TARGET = $(word 3, $(ARG))
 
 LINK_TARGET = $(shell pwd)/$(KEYBOARD)/$(KEYMAP)
-LINK_NAME = qmk_firmware/keyboards/$(KEYBOARD)/keymaps/$(KEYMAP)
+LINK_NAME = $(shell pwd)/qmk_firmware/keyboards/$(KEYBOARD)/keymaps/$(KEYMAP)
 
 all: compile
 
@@ -14,6 +14,7 @@ all: compile
 	ln -s ${LINK_TARGET} ${LINK_NAME}
 	make BUILD_DIR=$(shell pwd)/.build -j1 -C qmk_firmware $(KEYBOARD):$(KEYMAP):$(TARGET)
 	rm ${LINK_NAME}
+	rm -f ${LINK_TARGET}/${KEYMAP}
 
 clean:
 	rm -rf $(shell pwd)/.build
--- cradio/colemak-dh-swiss/keymap.c
+++ cradio/colemak-dh-swiss/keymap.c
@@ -15,115 +15,148 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+ // heavily inspired by
+ // - https://www.jonashietala.se/blog/2021/06/03/the-t-34-keyboard-layout/
+ // - https://github.com/callum-oakley/qmk_firmware/tree/master/users/callum
+
 #include QMK_KEYBOARD_H
 
-#define CH_AUML KC_QUOT       // ä umlaut
-#define CH_OUML KC_SCLN       // ö umlaut
-#define CH_UUML KC_LBRC       // ü umlaut
-#define CH_QUOT KC_MINS       // ' quote
-#define CH_CIRC KC_EQL        // ^ circumflex
-#define CH_TRMA KC_RBRC       // ¨ trema
-#define CH_DLR  KC_NUHS       // $ dollar sign
-#define CH_MINS KC_SLSH       // - minus sign
-#define CH_LT   KC_NUBS       // < lower than
-#define CH_PLUS LSFT(KC_1)    // + plus sign
-#define CH_DQT  LSFT(KC_2)    // " double quote
-#define CH_ASTR LSFT(KC_3)    // * asterisk
-#define CH_CCDL LSFT(KC_4)    // ç c with cedill
-#define CH_PERC LSFT(KC_5)    // % percent
-#define CH_AMPR LSFT(KC_6)    // & ampersand
-#define CH_SLSH LSFT(KC_7)    // / slash
-#define CH_LPRN LSFT(KC_8)    // ( left parentheses
-#define CH_RPRN LSFT(KC_9)    // ) right parentheses
-#define CH_EQL  LSFT(KC_0)    // = equal sign
-#define CH_QUES LSFT(CH_QUOT) // ? question mark
-#define CH_GRV  LSFT(CH_CIRC) // ` grave
-#define CH_EXLM LSFT(CH_TRMA) // ! exclamation mark
-#define CH_GT   LSFT(CH_LT)   // > greater than
-#define CH_SCLN LSFT(KC_COMM) // ; semicolon
-#define CH_COLN LSFT(KC_DOT)  // : colon
-#define CH_UNDS LSFT(CH_MINS) // _ underscore
-#define CH_AT   RALT(KC_2)    // @ at
-#define CH_HASH RALT(KC_3)    // # hash
-#define CH_PIPE RALT(KC_7)    // | pipe
-#define CH_BTCK RALT(CH_QUOT) // ´ backtick
-#define CH_TILD RALT(CH_CIRC) // ~ tilde
-#define CH_LBRC RALT(CH_UUML) // [ left bracket
-#define CH_RBRC RALT(CH_TRMA) // ] right bracket
-#define CH_LCBR RALT(CH_AUML) // { left curly bracket
-#define CH_RCBR RALT(CH_DLR)  // } right curly bracket
-#define CH_BSLS RALT(CH_LT)   // \ backslash
+#include "keymap_german_ch.h"       // https://github.com/qmk/qmk_firmware/blob/master/quantum/keymap_extras/keymap_german_ch.h
+#include "sendstring_german_ch.h"   // https://github.com/qmk/qmk_firmware/blob/master/quantum/keymap_extras/sendstring_german_ch.h
 
-#define CH_A    MT(CH_AUML, KC_A)   // a when tapped, ä when held
-#define CH_O    MT(CH_OUML, KC_O)   // o when tapped, ö when held
-#define CH_U    MT(CH_UUML, KC_U)   // u when tapped, ü when held
+// layer
+#define LA_SYM MO(SYM)
+#define LA_NAV MO(NAV)
+#define LA_GAM TO(GAM)
+
+// tap-hold
+#define TH_A LT(0, CH_A)
+#define TH_O LT(0, CH_O)
+#define TH_U LT(0, CH_U)
+
+enum layers {
+    DEF,
+    SYM,
+    NAV,
+    NUM,
+    GAM,
+};
+
+enum keycodes {
+    HEY = SAFE_RANGE,
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Colemak-DH Swiss
-    [0] = LAYOUT_split_3x5_2(
+    [DEF] = LAYOUT_split_3x5_2(
     //  -------------------------------------------------/**/--------------------------------------------------------
-        KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,        /**/         KC_J,    KC_L,    CH_U,    KC_Z,    KC_BSPC,
-        CH_A,    KC_R,    KC_S,    KC_T,    KC_G,        /**/         KC_M,    KC_N,    KC_E,    KC_I,    CH_O,
-        KC_Y,    KC_X,    KC_C,    KC_D,    KC_V,        /**/         KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
+        CH_Q,    CH_W,    CH_F,    CH_P,    CH_B,        /**/         CH_J,    CH_L,    TH_U,    CH_Z,    KC_BSPC,
+        TH_A,    CH_R,    CH_S,    CH_T,    CH_G,        /**/         CH_M,    CH_N,    CH_E,    CH_I,    TH_O,
+        CH_Y,    CH_X,    CH_C,    CH_D,    CH_V,        /**/         CH_K,    CH_H,    CH_COMM, CH_DOT,  CH_SLSH,
     //  -------------------------------------------------/**/--------------------------------------------------------
-        KC_SPC,   OSM(MOD_LSFT),                         /**/         OSM(MOD_RCTL),  TO(1)
+        MO(NAV), KC_LSFT,                                /**/         KC_SPC,  MO(SYM)
     //  -------------------------------------------------/**/--------------------------------------------------------
     ),
 
+    // Symbols
+    [SYM] = LAYOUT_split_3x5_2(
+    //  -------------------------------------------------/**/--------------------------------------------------------
+        _______, _______, _______, _______, _______,     /**/      _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______,     /**/      _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______,     /**/      _______, _______, _______, _______, _______,
+    //  -------------------------------------------------/**/--------------------------------------------------------
+        _______, _______,                                          _______,  _______
+    //  -------------------------------------------------/**/--------------------------------------------------------
+    ),
+
+
     // Navigation / Controls
-    [1] = LAYOUT_split_3x5_2(
+    [NAV] = LAYOUT_split_3x5_2(
     //  -------------------------------------------------/**/--------------------------------------------------------
         KC_TAB,  _______, _______, _______, _______,     /**/      _______, _______, _______, _______, _______,
         KC_ESC,  _______, _______, _______, _______,     /**/      _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,     /**/      _______, _______, _______, _______, _______,
     //  -------------------------------------------------/**/--------------------------------------------------------
-        TO(0),   _______,                                      _______,  TO(2)
+        _______, _______,                                /**/      _______, _______
     //  -------------------------------------------------/**/--------------------------------------------------------
     ),
 
-
-    // Symbols
-    [2] = LAYOUT_split_3x5_2(
+    // Numbers
+    [NUM] = LAYOUT_split_3x5_2(
     //  -------------------------------------------------/**/--------------------------------------------------------
         _______, _______, _______, _______, _______,     /**/      _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,     /**/      _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______,     /**/      _______, _______, _______, _______, _______,
     //  -------------------------------------------------/**/--------------------------------------------------------
-        TO(0),   _______,                           /**/           _______,  TO(3)
+        _______, _______,                                /**/      _______, _______
     //  -------------------------------------------------/**/--------------------------------------------------------
     ),
 
-
-    // GAMING
-    [3] = LAYOUT_split_3x5_2(
+    // Gaming
+    [GAM] = LAYOUT_split_3x5_2(
     //  -------------------------------------------------/**/--------------------------------------------------------
-        KC_TAB,  KC_G,    KC_Y,    KC_E,    KC_R,        /**/      _______, _______, _______, _______, _______,
-        MOD_LSFT,KC_A,    KC_W,    KC_D,    KC_F,        /**/      _______, _______, _______, _______, _______,
-        KC_X,    KC_Q,    KC_S,    KC_C,    KC_V,        /**/      _______, _______, _______, _______, _______,
+        KC_TAB,  CH_G,    CH_Y,    CH_E,    CH_R,        /**/      _______, _______, _______, _______, _______,
+        MOD_LSFT,CH_A,    CH_W,    CH_D,    CH_F,        /**/      _______, _______, _______, _______, _______,
+        CH_X,    CH_Q,    CH_S,    CH_C,    CH_V,        /**/      _______, _______, _______, _______, _______,
     //  -------------------------------------------------/**/--------------------------------------------------------
-        TO(0),   MOD_LSFT,                               /**/      _______, _______
+        TO(0),   _______,                                /**/      _______, _______
     //  -------------------------------------------------/**/--------------------------------------------------------
     ),
 
-
-    /*[1] = LAYOUT_split_3x5_2(
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,                _______, KC_7,    KC_8,    KC_9,    _______,
-    KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,                _______, KC_4,    KC_5,    KC_6,    KC_0,
-    KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,                _______, KC_1,    KC_2,    KC_3,    _______,
-                               TO(0),   _______,                KC_ENT,  _______
-    ),
-    [2] = LAYOUT_split_3x5_2(
-    _______, _______, _______, _______, _______,                _______, _______, _______, _______, _______,
-    // < [ ( {      } ) ] >
-    KC_NUBS, RALT(KC_LBRC), LSFT(KC_8), RALT(KC_QUOT), _______, _______, RALT(KC_NUHS), LSFT(KC_9), RALT(KC_RBRC), LSFT(KC_NUBS),
-    _______, _______, _______, _______, _______,                _______, _______, _______, _______, _______,
-                               TO(0), _______,                _______,  MO(3)
-    ),
-    [3] = LAYOUT_split_3x5_2(
-    _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______,
-                               TO(0), _______,     _______, _______
-    )*/
 };
 
+// change the hold function
+// https://docs.qmk.fm/#/mod_tap?id=changing-hold-function
+bool intercept_hold(keyrecord_t *record, uint16_t keycode) {
+    if (!record->tap.count && record->event.pressed) {
+        tap_code(keycode);
+        return false;
+    }
+    return true;
+}qqaarräaaassssssäaaaaaa a   aaaaarrrrststsrstt
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case TH_A:
+            return intercept_hold(record, CH_ADIA);
+        case TH_O:
+            return intercept_hold(record, CH_ODIA);
+        case TH_U:
+            return intercept_hold(record, CH_UDIA);
+    }
+    return true;
+}
+
+void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // 
+}
+
+void matrix_scan_user(void) {
+    // can be used for timers
+}
+
+
+/*bool is_oneshot_cancel_key(uint16_t keycode) {
+    switch (keycode) {
+    case LA_SYM:
+    case LA_NAV:
+        return true;
+    default:
+        return false;
+    }
+}
+
+bool is_oneshot_ignored_key(uint16_t keycode) {
+    switch (keycode) {
+    case LA_SYM:
+    case LA_NAV:
+    case KC_LSFT:
+    case OS_SHFT:
+    case OS_CTRL:
+    case OS_ALT:
+    case OS_CMD:
+        return true;
+    default:
+        return false;
+    }
+}*/
